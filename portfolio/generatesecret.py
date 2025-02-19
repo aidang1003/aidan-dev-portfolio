6c74cf9562496bd797895af6fb5a73061d73c321
aidang1003 <60905689+aidang1003@users.noreply.github.com>
2025-02-19 07:35:56 -0700

    testing key w env variable

--- portfolio/generatesecret.py
+++ portfolio/generatesecret.py
@@ -1,3 +1,9 @@
 import secrets
+import os
+
 # Generates a secret key for production deployment
 print(secrets.token_hex(24))
+
+
+print(os.getenv('$TEST_SECRET_KEY'))
+print(os.environ.get('TEST_SECRET_KEY'))
