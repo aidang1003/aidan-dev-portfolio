2379e909586c1f664516c0e09bad40e52681faa0
Aidan Gorny <60905689+aidang1003@users.noreply.github.com>
2026-02-17 23:03:25 -0700

    Delete Portfolio directory
    Lowercase folder name

--- Portfolio/contact.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, redirect
-
-contact_bp = Blueprint("contact", __name__)
-
-@contact_bp.route('/contact', methods=["GET","POST"])
-def contact():
-    return redirect('/#contact')
